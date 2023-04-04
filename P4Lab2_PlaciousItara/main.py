e44727a1f292c8d47712f76898c17a00d0e5a872
placioui5221 <124919434+placioui5221@users.noreply.github.com>
2023-04-04 13:59:51 -0400

    Add files via upload

---
+++ P4Lab2_PlaciousItara/main.py
@@ -0,0 +1,12 @@
+''' Type your code here. '''
+num1 = int(input())
+num2 = int(input())
+
+if num2 < num1:
+    print("Second integer can't be less than the first.")
+else:
+    for i in range(num1, num2 + 1, 5):
+        print(f'{ str(i) } ', end= '')
+    print()
+
+    
